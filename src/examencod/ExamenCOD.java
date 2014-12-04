0a9b049d59dc1ca03fa43ecf92c76c14bc03e0b4
gpineiroblanco <gpineiroblanco@eniac14.danielcastelao.org>
2014-12-04 09:16:06 +0100

    Cambio de nombre de variables y métodos. Cmabio el nombre de la variable iN por iN1 y el nombre del método metodoMio() por metodoTuyo().

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -7,18 +7,18 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- int iN = 11;
-            	if (metodoMio(iN))
+ int iN1 = 11;
+            	if (metodoTuyo(iN1))
 System.out.println("Es numero primo");
 else
  System.out.println("NO es numero primo");           	 
           	 
 for (int x=2;x<1000;x++) {
-if (metodoMio(x))
+if (metodoTuyo(x))
 System.out.print(x + " ");                         	 
 }         	 
 	}
-public static boolean metodoMio(int variable){
+public static boolean metodoTuyo(int variable){
 int j = 2;
 boolean aux=true;
 while ((aux) && (j!=variable)){
